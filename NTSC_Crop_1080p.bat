c127727a0b80c70bf57e47dc96e92ee023712eb6
Climatee <102872635+Climatee@users.noreply.github.com>
2023-09-14 17:26:06 -0400

    Update NTSC_Crop_1080p.bat

--- NTSC_Crop_1080p.bat
+++ NTSC_Crop_1080p.bat
@@ -1,7 +1,7 @@
 @echo off
 set /p start="Start time (HH:MM:SS): "
 set /p end="End time (HH:MM:SS): "
-.\ffmpeg -ss %start% -to %end% -i "%~1" -to %end% -vf scale=336:252:flags=neighbor,crop=312:237:12:9,scale=1440:1080:flags=neighbor -c:v libx264 -crf 14 -preset slower -pix_fmt yuv444p -acodec copy -r (27700/926) "%~1".mp4 
+.\ffmpeg -ss %start% -to %end% -i "%~1" -to %end% -vf scale=336:252:flags=neighbor,crop=312:237:12:9,scale=1440:1080:flags=neighbor -c:v libx264 -crf 14 -preset slow -pix_fmt yuv444p -acodec copy -r (27700/926) "%~1".mp4 
 
 
 REM Luke Dischave
