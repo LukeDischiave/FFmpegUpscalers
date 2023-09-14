6c13dd7d8306118060e03241d3944d46ab5eb9c8
Climatee <102872635+Climatee@users.noreply.github.com>
2023-09-14 17:26:21 -0400

    Update PAL_Crop_1080p.bat

--- PAL_Crop_1080p.bat
+++ PAL_Crop_1080p.bat
@@ -3,4 +3,4 @@ set /p start="Start time (HH:MM:SS): "
 set /p end="End time (HH:MM:SS): "
 .\ffmpeg -ss %start% -to %end% -i "%~1" -to %end% -vf scale=336:300:flags=neighbor,crop=312:237:15:36,scale=1680x1080:flags=neighbor -c:v libx264 -crf 13 -preset slow -pix_fmt yuv444p -acodec copy -r (17722/710) "%~1".mp4
 
-REM Luke Dischiave, 1350x1080
+REM Luke Dischiave
